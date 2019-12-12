075646282359a948371b52b4b21bec11649b4740
Ben Dwyer <ben@scruffian.com>
2019-12-12 15:16:59 +0000

    [not verified] display the block in the front end

--- extensions/blocks/opentable/opentable.php
+++ extensions/blocks/opentable/opentable.php
@@ -15,12 +15,34 @@ jetpack_register_block(
 /**
  * Opentable block registration/dependency declaration.
  *
- * @param array  $attr    Array containing the Opentable block attributes.
- * @param string $content String containing the Opentable block content.
+ * @param array $attributes    Array containing the Opentable block attributes.
  *
  * @return string
  */
-function jetpack_opentable_block_load_assets( $attr, $content ) {
+function jetpack_opentable_block_load_assets( $attributes ) {
 	Jetpack_Gutenberg::load_assets_as_required( 'opentable' );
-	return $content;
+
+	return '<script type="text/javascript" src="' . esc_url( jetpack_opentable_build_embed_url( $attributes ) ) . '"></script>';
+}
+
+/**
+ * Build an embed URL from an array of URL values.
+ *
+ * @param array $attributes Array of URL values.
+ *
+ * @return string Embed URL
+ */
+function jetpack_opentable_build_embed_url( $attributes ) {
+	return add_query_arg(
+		array(
+			'rid'    => $attributes['rid'],
+			'type'   => $attributes['type'],
+			'theme'  => $attributes['theme'],
+			'iframe' => $attributes['iframe'],
+			'domain' => $attributes['domain'],
+			'lang'   => $attributes['lang'],
+			'newtab' => $attributes['newtab'],
+		),
+		'//www.opentable.com/widget/reservation/loader'
+	);
 }
