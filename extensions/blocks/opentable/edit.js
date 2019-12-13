582577dc8773c1dbf9f73bf90e4e2321fb6f8657
Paul Bunkham <paul@dobit.co.uk>
2019-12-13 01:44:02 +0000

    Fix the RID input event by swapping to onChange

--- extensions/blocks/opentable/edit.js
+++ extensions/blocks/opentable/edit.js
@@ -125,7 +125,7 @@ export default function OpentableEdit( {
 				<input
 					type="text"
 					value={ rid }
-					onBlur={ event => setAttributes( { rid: event.target.value } ) }
+					onChange={ event => setAttributes( { rid: event.target.value } ) }
 				/>
 				<br />
 				theme:
